753273aba48c7537165c62673a74f78c460c61e3
Sammyrn <128840880+Sammyrn@users.noreply.github.com>
2024-11-17 04:10:11 +0100

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const btn = document.querySelector('.menu-btn');
 const dropdown = document.querySelector('.hamburger');
 const lightbtn = document.querySelector('.light');
 const body = document.body;
+const box = document.querySelectorAll('.box');
+window.addEventListener('scroll', check_animation)
+
+check_animation()
+
+function check_animation(){
+    const trigger = window.innerHeight / 6 * 4;
+    box.forEach(box => {
+        const top = box.getBoundingClientRect().top;
+
+        if (trigger > top){
+            box.classList.add('activate')
+        } else{
+            box.classList.remove('activate')
+        }
+    })
+};
 
 function toggle(){
     btn.addEventListener('click', function(){
@@ -14,5 +31,6 @@ function toggle(){
     });
     
 };
+
 
 toggle();
